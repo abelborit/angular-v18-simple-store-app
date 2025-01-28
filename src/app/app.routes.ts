c260c3f1439fe69edcba403ae2226805b425d5d8
Abel Borit <abelboritguitton@gmail.com>
2025-01-27 22:35:47 -0500

    [rename] folder "features" to "components"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,11 @@ export const routes: Routes = [
   {
     path: '',
     /* FORMA 1: exportación por defecto del componente y ya no sería necesario usar promesas */
-    // loadComponent: () => import('./products/features/product-shell/product.routes'),
+    // loadComponent: () => import('./products/components/product-shell/product.routes'),
 
     /* FORMA 2: como promesa y ya no sería necesaria la exportación por defecto del componente */
     loadChildren: () =>
-      import('./products/features/product-shell/product.routes').then(
+      import('./products/components/product-shell/product.routes').then(
         (module) => module.productRoutes
       ),
   },
