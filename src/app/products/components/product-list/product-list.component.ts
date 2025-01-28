58076853566c780494f0398a0558c84027ae3a72
Abel Borit <abelboritguitton@gmail.com>
2025-01-27 23:18:50 -0500

    [add] BaseHttpService in ProductsService

--- src/app/products/components/product-list/product-list.component.ts
+++ src/app/products/components/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
+import { ProductInterface } from '../../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-product-list',
@@ -9,13 +10,31 @@ import { ProductsService } from '../../services/products.service';
   styleUrl: './product-list.component.scss',
   providers: [ProductsService], // se coloca aquí ya que estamos haciendo que este servicio sea usado solo para este componente, si se colocara como -- providedIn: 'root', -- entonces no haría falta colocar este "providers" ya que se podría usar en cualquier parte de la aplicación
 })
-export class ProductListComponent {
-  public productsService = inject(ProductsService);
+export class ProductListComponent implements OnInit {
+  private productsService = inject(ProductsService);
 
-  constructor() {
-    this.productsService.getAllProducts().subscribe((products) => {
-      console.log(products);
-      return;
+  public products: ProductInterface[] = [];
+
+  // constructor() {
+  //   this.productsService.getAllProducts().subscribe((products) => {
+  //     console.log(products);
+  //     return;
+  //   });
+  // }
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  private getAllProducts(): void {
+    this.productsService.getAllProducts().subscribe({
+      next: (data) => {
+        this.products = data; // Procesa los productos obtenidos
+        console.log('Products loaded:', data);
+      },
+      error: (err) => {
+        console.error('Error loading products:', err);
+      },
     });
   }
 }
--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,17 +1,23 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { BaseHttpService } from '../../shared/services/base-http.service';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { ProductInterface } from '../../shared/interfaces/product.interface';
 
 /* se comenta esto ya que en este caso no haremos que sea proveído en root de toda la aplicación porque solo se usará en la parte de products */
 // @Injectable({
 //   providedIn: 'root',
 // })
 @Injectable()
-export class ProductsService {
-  constructor() {}
+export class ProductsService extends BaseHttpService {
+  private readonly baseApiUrl = environment.BASE_API_URL;
 
-  private httpClient = inject(HttpClient);
-
-  public getAllProducts() {
-    return this.httpClient.get('https://fakestoreapi.com/products');
+  /**
+   * Obtiene todos los productos desde la API.
+   * Utiliza el método `get` de BaseHttpService para mayor robustez.
+   * @returns Un Observable con la lista de productos.
+   */
+  public getAllProducts(): Observable<ProductInterface[]> {
+    return this.getBaseHttp(this.baseApiUrl, 'products'); // Usa el método genérico 'get' de BaseHttpService
   }
 }
