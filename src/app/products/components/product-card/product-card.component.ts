ac5150bb0d94d8d2b02d45973e805f1be5c47268
Abel Borit <abelboritguitton@gmail.com>
2025-02-03 22:02:05 -0500

    [update] product-card.component

--- src/app/products/components/product-card/product-card.component.ts
+++ src/app/products/components/product-card/product-card.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ProductInterface } from '../../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
   imports: [],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
-
+  public productInfo = input.required<ProductInterface>();
 }
